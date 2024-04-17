4b4cf17e547173c50e0d0f41e687cb81eb4f1474
MariaCancian <maria.cancian@aluno.senai.br>
2024-04-17 14:07:43 -0300

    termino de notificações

--- App.jsx
+++ App.jsx
@@ -1,5 +1,37 @@
-import Routes from "./src/Routes";
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
-export default function App(){
-  return <Routes/>;
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export default function App() {
+  useEffect(() => {
+    scheduleDailyNotification();
+  }, []);
+
+  async function scheduleDailyNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Bom dia!",
+        body: 'Não se esqueça de lavar a careca.',
+      },
+      trigger: {
+        hour: 11, // Defina a hora desejada para a notificação (no formato de 24 horas)
+        minute: 45, // Defina os minutos desejados para a notificação
+        repeats: true, // Notificação será repetida diariamente
+      },
+    });
+  }
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Notificação diária será agendada automaticamente!</Text>
+    </View>
+  );
 }
